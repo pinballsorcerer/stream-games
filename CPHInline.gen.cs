858747c19ee8b25b741a8838dd49c4d0c2ce546e
pinballsorcerer <pinballsorcerer@outlook.com>
2022-07-16 16:50:54 -0700

    Document args; dice command shows invoking user

--- CPHInline.gen.cs
+++ CPHInline.gen.cs
@@ -1,6 +1,41 @@
+using System.Collections.Generic;
 using Plugins;
 
 public partial class CPHInline
 {
+    /*
+    Known 'args' keys:
+    command
+    rawInput
+    rawInputEscaped
+    rawInputUrlEncoded
+    input0
+    inputEscaped0
+    inputUrlEncoded0
+    counter
+    userCounter
+    user
+    userName
+    userId
+    isSubscribed
+    isModerator
+    isVip
+    broadcastUser
+    broadcastUserName
+    broadcastUserId
+    broadcasterIsAffiliate
+    broadcasterIsPartner
+    randomUser0
+    randomUserName0
+    randomUserId0
+    randomUser1
+    randomUserName1
+    randomUserId1
+    randomUser2
+    randomUserName2
+    randomUserId2
+    */
+    protected Dictionary<string, object> args;
+
     protected InlineInvokeProxy CPH { get; private set; }
 }
--- Dice.cs
+++ Dice.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Plugins;
 
 namespace Pinball
@@ -16,8 +17,20 @@ namespace Pinball
         {
             var random = new Random();
             var diceResult = random.Next(1, 21);
+            string user = "You";
 
-            Proxy.SendMessage($"You rolled a d20: and got a {diceResult}!", bot: true);
+            Proxy.SendMessage($"{user} rolled a d20: and got a {diceResult}!", bot: true);
+
+            return ValueTuple.Create();
+        }
+
+        public static ValueTuple RollD20(InlineInvokeProxy proxy, Dictionary<string,object> args)
+        {
+            var random = new Random();
+            var diceResult = random.Next(1, 21);
+            string user = args != null && args.ContainsKey("user") ? args["user"]?.ToString() : "You";
+
+            proxy.SendMessage($"{user} rolled a d20: and got a {diceResult}!", bot: true);
 
             return ValueTuple.Create();
         }
